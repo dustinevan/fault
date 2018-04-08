8fc9feb4825110a6c0298011c5e1ee0979f0c8a9
dustinevan <dustinevancurrie@gmail.com>
2018-04-07 22:17:39 -0600

    remove the extra stuff

--- error.go
+++ error.go
@@ -1,15 +1,8 @@
 package fault
 
 import (
-	"fmt"
-
 	"github.com/pkg/errors"
-	"strconv"
-	"github.com/json-iterator/go"
-	"net/http"
 )
-
-var json = jsoniter.ConfigCompatibleWithStandardLibrary
 
 // This interface allows use of the Cause method via type assertion. errors created by
 // errors.Wrap, errors.Wrapf, errors.WithMessage, or errors.WithStack implement this interface
@@ -21,14 +14,42 @@ type causer interface {
 // errors.Wrap, errors.Wrapf, errors.WithStack implement this interface
 type stackTracer interface {
 	StackTrace() errors.StackTrace
-	error
 }
 
-// Interfaces that inherit cause are discoverable during the cause chain loop. See laag.HttpCode(err)
-type httpCodeError interface {
-	HttpCode() (int, bool)
+// httpStatus is an error that has a recommended http status associated with it
+type httpStatuser interface {
+	HttpStatus() int
 	causer
-	error
+}
+
+// if an alerter exists in the error tree, the entire error should be sent as an alert.
+type alerter interface {
+	Alert()
+	causer
+}
+
+type httpStatus struct {
+	status int
+	err    error
+}
+
+func (h httpStatus) HttpStatus() int {
+	return h.status
+}
+
+func (h httpStatus) Cause() error {
+	return h.err
+}
+
+func (h httpStatus) Error() string {
+	return h.err.Error()
+}
+
+func WithHttpStatus(err error, status int) error {
+	return httpStatus{
+		status: status,
+		err:    err,
+	}
 }
 
 func HttpStatus(err error) (int, bool) {
@@ -37,218 +58,49 @@ func HttpStatus(err error) (int, bool) {
 		if !ok {
 			return 0, false
 		}
-		h, ok := c.(httpCodeError)
+		h, ok := c.(httpStatuser)
 		if !ok {
 			err = c.Cause()
 			continue
 		}
-		return h.HttpCode()
+		return h.HttpStatus(), true
 	}
 	return 0, false
 }
 
-type typ int
-
-const (
-	NoLog typ = iota
-	Success
-	Info
-	Err
-	Bug
-	SysFailure
-)
-
-var typs = [...]string{
-	"no log needed",
-	"success",
-	"info",
-	"error",
-	"bug",
-	"system failure",
+type alertErr struct {
+	err error
 }
 
-func (t typ) String() string {
-	return typs[int(t)]
-}
-
-func (t *typ) UnmarshalJSON(b []byte) error {
-	for i, p := range typs {
-		if p == string(b) {
-			*t = typ(i)
-			return nil
-		}
-	}
-	*t = NoLog
-	return nil
-}
-
-func (t *typ) MarshalJSON() ([]byte, error) {
-	return []byte("\"" + t.String() + "\""), nil
-}
-const OpeningAlertTag = "\n<alert>\n"
-const ClosingAlertTag = "\n</alert>\n"
-
-type AlertTag struct {
-	Msg       string
-	Type       typ
-	Subsystem string
-}
-
-type ErrOption func(*appError)
-
-func WithTrace() ErrOption {
-	return func(a *appError) {
-		a.includeTrace = true
+func WithAlert(err error) error {
+	return &alertErr{
+		err: err,
 	}
 }
 
-func Alert(subsystem string, tag typ) ErrOption {
-	return func(a *appError) {
-		a.isAlert = true
-		a.subsystem = subsystem
-		a.tag = tag
-	}
+func (a alertErr) Error() string {
+	return a.err.Error()
 }
 
-func Type(t typ) ErrOption {
-	return func(a *appError) {
-		a.tag = t
-	}
-}
-
-func System(s string) ErrOption {
-	return func(a *appError) {
-		a.subsystem = s
-	}
-}
-
-func HttpCode(h int) ErrOption {
-	return func(a *appError) {
-		a.httpcode = h
-	}
-}
-
-func HttpUnauthorized() ErrOption {
-	return HttpCode(http.StatusUnauthorized)
-}
-
-func HttpServerError() ErrOption {
-	return HttpCode(http.StatusInternalServerError)
-}
-
-func HttpBadRequest() ErrOption {
-	return HttpCode(http.StatusBadRequest)
-}
-
-func HttpNotFound() ErrOption {
-	return HttpCode(http.StatusNotFound)
-}
-
-func DontLog() ErrOption {
-	return Type(NoLog)
-}
-
-func LogSuccess() ErrOption {
-	return Type(Success)
-}
-func LogInfo() ErrOption {
-	return Type(Info)
-}
-func LogError() ErrOption {
-	return Type(Err)
-}
-func LogBug() ErrOption {
-	return Type(Bug)
-}
-
-func LogFailure() ErrOption {
-	return Type(SysFailure)
-}
-
-type appError struct {
-	err          stackTracer
-	tag          typ
-	subsystem    string
-	httpcode     int
-	includeTrace bool
-	isAlert      bool
-}
-
-func Error(err error, opts ...ErrOption) error {
-	st, ok := err.(stackTracer)
-	if !ok {
-		e := errors.WithStack(err)
-		a := &appError{
-			err: e.(stackTracer),
-			tag: Err,
-		}
-		for _, opt := range opts {
-			opt(a)
-		}
-		return a
-	}
-	a := &appError{
-		err: st,
-		tag: Err,
-	}
-	for _, opt := range opts {
-		opt(a)
-	}
-	return a
-}
-
-func (a *appError) Error() string {
-	e := a.builderror()
-	if a.isAlert == true {
-		b, err := json.Marshal(a.Alert())
-		if err != nil {
-			return e + ": unable to marshal alert: " + err.Error()
-		}
-		return e + OpeningAlertTag + string(b) + ClosingAlertTag
-	}
-	return e
-}
-
-func (a *appError) builderror() string {
-	if a.tag == NoLog {
-		return ""
-	}
-	e := fmt.Sprintf("%s: %s", a.tag, a.err.Error())
-	if a.subsystem != "" {
-		e = a.subsystem + ": " + e
-	}
-	if a.httpcode != 0 {
-		e += ": http code " + strconv.Itoa(a.httpcode)
-	}
-	if a.includeTrace == true {
-		e += "\n" + a.Trace()
-	}
-	return e
-}
-
-func (a *appError) Trace() string {
-	s := ""
-	for _, f := range a.err.StackTrace() {
-		s += fmt.Sprintf("%+v:", f)
-	}
-	return s
-}
-
-func (a *appError) Alert() AlertTag {
-	return AlertTag{
-		Msg: a.builderror(),
-		Type: a.tag,
-		Subsystem: a.subsystem,
-	}
-}
-
-func (a *appError) Cause() error {
+func (a alertErr) Cause() error {
 	return a.err
 }
 
-func (a *appError) HttpCode() (int, bool) {
-	if a.httpcode == 0 {
-		return 0, false
+func (a alertErr) Alert() {
+}
+
+func IsAlert(err error) bool {
+	for err != nil {
+		c, ok := err.(causer)
+		if !ok {
+			return false
+		}
+		_, ok = c.(alerter)
+		if !ok {
+			err = c.Cause()
+			continue
+		}
+		return true
 	}
-	return a.httpcode, true
+	return false
 }
--- errors_test.go
+++ errors_test.go
@@ -1,30 +1,56 @@
 package fault
 
 import (
-	"testing"
-	"fmt"
-	"github.com/pkg/errors"
 	"net/http"
-	"strings"
+	"testing"
+
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestHttpCode(t *testing.T) {
-	orig := errors.Wrap(fmt.Errorf("db error"), "found this")
-	web := Error(orig, HttpCode(http.StatusInternalServerError), WithTrace())
-	//fmt.Println(web)
-	//fmt.Println(HttpStatus(web))
-	alert := Error(web, Alert("Database", SysFailure))
-	//fmt.Println(alert)
-	trace := Error(alert, WithTrace())
-	msg := trace.Error()
-	//fmt.Println(msg)
-	i := strings.Index(msg, OpeningAlertTag)
-	j := strings.Index(msg, ClosingAlertTag)
-	alertjson := strings.Trim(msg[i+len(OpeningAlertTag)-1:j], "\n")
-	var a AlertTag
-	err := json.Unmarshal([]byte(alertjson), &a)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(a.Subsystem, "|", a.Type, "|", a.Msg)
+func TestWithHttpStatus(t *testing.T) {
+	cause := errors.New("db connection error")
+	wrap1 := errors.Wrap(cause, "1st wrap")
+	withHttp := WithHttpStatus(wrap1, http.StatusInternalServerError)
+	wrap2 := errors.Wrap(withHttp, "2nd wrap")
+	wrap3 := errors.Wrap(wrap2, "3rd wrap")
+
+	assert.Equal(t, cause, errors.Cause(wrap3))
+	status, ok := HttpStatus(wrap3)
+	assert.True(t, ok)
+	assert.Equal(t, http.StatusInternalServerError, status)
+
+	assert.Equal(t, cause, errors.Cause(withHttp))
+	status, ok = HttpStatus(wrap1)
+	assert.False(t, ok)
+	assert.Equal(t, 0, status)
+
+	assert.Equal(t, "3rd wrap: 2nd wrap: 1st wrap: db connection error", wrap3.Error())
+
+	status, ok = HttpStatus(nil)
+	assert.False(t, ok)
+	assert.Equal(t, 0, status)
+}
+
+func TestWithAlert(t *testing.T) {
+	cause := errors.New("db connection error")
+	wrap1 := errors.Wrap(cause, "1st wrap")
+	withAlert := WithAlert(wrap1)
+	wrap2 := errors.Wrap(withAlert, "2nd wrap")
+	wrap3 := errors.Wrap(wrap2, "3rd wrap")
+
+	assert.Equal(t, cause, errors.Cause(wrap3))
+	ok := IsAlert(wrap3)
+	assert.True(t, ok)
+
+	assert.Equal(t, cause, errors.Cause(withAlert))
+	ok = IsAlert(wrap1)
+	assert.False(t, ok)
+
+	assert.Equal(t, "3rd wrap: 2nd wrap: 1st wrap: db connection error", wrap3.Error())
+
+	assert.Equal(t, cause, errors.Cause(withAlert))
+	ok = IsAlert(nil)
+	assert.False(t, ok)
+
 }
